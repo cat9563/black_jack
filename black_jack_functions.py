fb66a8d38af74921818a59ff1d9e87c114370304
Crystal Ramirez <crystal9563@gmail.com>
2020-07-04 09:25:44 -0400

    fixed take_bets function to return the value and added error handling for user input

--- black_jack_functions.py
+++ black_jack_functions.py
@@ -43,68 +43,82 @@ def hand(myDeck):
     return players_hand, dealers_hand
 
 # working on these feature soon 
-# def error_handling():
+def error_handling():
+    betting_rules = ("House Rules\nBets must be greater than $5\nNo change excepetd only dollars y'all!\n")
+    print(betting_rules)
+    while True:
+        num = input("How much woud you like to bet: ")
+        try:
+            val = int(num)
+            return val
+        except ValueError:
+            print("Less talk more give.\n")
 
 def take_bets(bet):
-    balance = 100
+    balance = 200 
     users_bet = bet 
 
-    if users_bet > balance:
-        print("you don't have that much in your account.\nYou might want to head out to the pawnshop or work on your corner!") 
-    elif users_bet < 5:
-        print( "common man you don't have $5, seriously you need help!\nWe only take bets greater than $5!!")
-    else:
-        print("GOOD LUCK CHAMP!")
-        print(f"Thanks for your donation of ${users_bet}. The house appreciates you.")
- 
-# def double_down():
+    while True:
+        balance = 200
+        users_bet = error_handling()
+        if users_bet > balance:
+            print("you don't have that much in your account.\nYou might want to head out to the pawnshop or work the corner!\n") 
+        elif users_bet < 5:
+            print( "common man you don't have $5, seriously you need help!\nWe only take bets greater than $5!!\n")
+        else:
+            print("GOOD LUCK CHAMP!")
+            print(f"Thanks for your donation of ${users_bet}. The house appreciates you.\n")
+            return users_bet
+
+take_bets(error_handling())
+# # def double_down():
     
 
-# def split_hand():
+# # def split_hand():
     
 
 
-game logic 
+# #game logic 
  
-myDeck = dealers_deck()
-hands = hand(myDeck)
-dealer = hands[1]
-player = hands[0]
+# myDeck = dealers_deck()
+# hands = hand(myDeck)
+# dealer = hands[1]
+# player = hands[0]
 
-# keep track of the players score and the dealers score 
-players_score = 0
-dealers_score = 0
+# # keep track of the players score and the dealers score 
+# players_score = 0
+# dealers_score = 0
 
-# let the player know the game is about to begin
-prompt = "Welcome to wonderful game of BlackJack will you make it bigtime or find yourself in the rags. Only time will tell"
-print(prompt)
+# # let the player know the game is about to begin
+# prompt = "Welcome to wonderful game of BlackJack will you make it bigtime or find yourself in the rags. Only time will tell"
+# print(prompt)
 
-while players_score <= 21:
-    playerCount = count_points(player)
-    dealerCount = count_points(dealer)
-    print(f"The dealer has: {dealer[0]} in their hand")
-    print(f"The player has: {player[0]} {player[1]} in your hand")
-    print(playerCount)
-    print(dealerCount)
-    prompt = input('h: hit, s:stand')
+# while players_score <= 21:
+#     playerCount = count_points(player)
+#     dealerCount = count_points(dealer)
+#     print(f"The dealer has: {dealer[0]} in their hand")
+#     print(f"The player has: {player[0]} {player[1]} in your hand")
+#     print(playerCount)
+#     print(dealerCount)
+#     prompt = input('h: hit, s:stand')
 
-    if playerCount == 21:
-        print("Blackjack good one you win!")
-        break
-    elif playerCount > 21:
-        print("To bad so sad. You lose!!!")
-        break
-    elif dealerCount > 21:
-        print("The dealer has lost congrats your one step closer to making your dreams come true.\nDon't worry about the stress you are causing on your family!")
-        break
+#     if playerCount == 21:
+#         print("Blackjack good one you win!")
+#         break
+#     elif playerCount > 21:
+#         print("To bad so sad. You lose!!!")
+#         break
+#     elif dealerCount > 21:
+#         print("The dealer has lost congrats your one step closer to making your dreams come true.\nDon't worry about the stress you are causing on your family!")
+#         break
 
-    if prompt == "h":
-        player.append(myDeck.pop())
-        players_score = playerCount
-        print(players_score)
-    elif playerCount > dealerCount:
-        print(f"you win with {playerCount} points")
-        break
-    else:
-        print(f"Dealer wins with {dealerCount} points")
-        break
+#     if prompt == "h":
+#         player.append(myDeck.pop())
+#         players_score = playerCount
+#         print(players_score)
+#     elif playerCount > dealerCount:
+#         print(f"you win with {playerCount} points")
+#         break
+#     else:
+#         print(f"Dealer wins with {dealerCount} points")
+#         break
