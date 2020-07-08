4846617a579bded599f2d0744690d834812c6846
Crystal Ramirez <crystal9563@gmail.com>
2020-07-08 10:49:50 -0400

    uses dicts and list to evualte sum of players hand

--- black_jack_functions.py
+++ black_jack_functions.py
@@ -4,6 +4,9 @@ import sys
 # using this as exercise to work on functions will be working towards turning this into a class. 
 
 def create_deck(rank):
+    """
+    Creates a deck when give a list.
+    """
     deck = []
     suites = ['H','S','D','C']
     for i in suites:
@@ -12,10 +15,24 @@ def create_deck(rank):
     return deck
 
 def Merge(dict1, dict2, dict3): 
+    """
+    merges 3 dictionary together 
+    """
     res = {**dict1, **dict2, **dict3} 
     return res
 
+def add_card_to_hand(hand,new_card):
+    """
+    adds new card to hand
+    """
+    new_hand = {**hand, **new_card}
+    new_hand_values = new_hand.values()
+    return new_hand , new_hand_values
+
 def shuffle_deck():
+    """
+    Shuffles a dictionary and creates a shuffled deck. 
+    """
     keys =  list(completDict.keys())
     random.shuffle(keys)
 
@@ -24,23 +41,34 @@ def shuffle_deck():
         ShuffledcompletDict.update({key:completDict[key]})
     return ShuffledcompletDict
 
-def deal_hand(ShuffledcompletDict):    
+def deal_hand(ShuffledcompletDict, n):
+    """
+    Deals out hand 
+    """    
     # Initialize limit  
-    N = 2
+    N = n
     player = dict(list(ShuffledcompletDict.items())[0: N])
-    dealer = dict(list(ShuffledcompletDict.items())[0: N])
-    return player, dealer 
+    return player
 
 def create_dict(k, v):
+    """
+    Create a dictinary from a list 
+    """
     card_dict = {k[i]: v[i] for i in range(len(k))} 
     return card_dict
 
 def sum_points(hand):
+    """
+    Creates a sum value by evaluating the key pair in the players hand. 
+    """
     values = hand.values()
     total = sum(values)
-    return total
+    return total, values
 
 def error_handling():
+    """
+    Checks if user inserted the correct information and keeps them in a loop unitl they do. 
+    """
     betting_rules = ("House Rules\nBets must be greater than $5\nNo change excepetd only dollars y'all!\n")
     print(betting_rules)
     while True:
@@ -52,11 +80,11 @@ def error_handling():
             print("Less talk more give.\n")
         print(val)
 
-def take_bets():
-    balance = 200 
+def take_bets(b):
+    balance = b
 
     while True:
-        balance = 200
+        balance = b
         users_bet = error_handling()
         if users_bet > balance:
             print("you don't have that much in your account.\nYou might want to head out to the pawnshop or work the corner!\n") 
@@ -70,6 +98,24 @@ def take_bets():
 def double_down(double_down):
     bet = double_down
     return bet * 2
+
+def dealing_with_aces(ph):
+    total_and_value  = sum_points(ph)
+    total = total_and_value[0]
+    value = total_and_value[1]
+    new_total = []
+
+    if total == 21:
+        print("Player __ is the winner with black jack")
+
+    if total > 21:
+        for i in value:
+            new_total.append(i)
+        for i, num in enumerate(new_total):
+            if num == 11:
+                new_total[i] = 1
+                new_totals = sum(new_total)
+        return new_totals
 
 
 #######################################################################
@@ -88,8 +134,85 @@ no_faceCardsDict = create_dict(no_faceCards, no_faceValues)
 faceCardDict = create_dict(faceCards, faceCarValues)
 aceCardDict = create_dict(aceCards, aceValues)
 
-#created a dict then converts to a list of tuples  
+#created a complete dictionary  
 completDict = Merge(no_faceCardsDict, faceCardDict, aceCardDict)
-completeTuple = list(completDict.items())
 
 #######################################################################
+
+############################################################
+# Provied Logic for Player and Dealer Hand                 #
+############################################################
+
+# get some player hands 
+players_hand = deal_hand(shuffle_deck(), 2)
+dealers_hand = deal_hand(shuffle_deck(), 2)
+
+
+#account for points if value is 21 part of first check
+player_points = sum_points(players_hand)
+dealers_points = sum_points(dealers_hand)
+
+
+# accounting for points when the value is less than 21 
+
+
+
+#Dealing with aces
+values = players_hand.values()
+new_hand_total = sum(values)
+
+
+# for i in values:
+#     players_hand_initial_pair.append(i)
+# print(players_hand_initial_pair)
+# print(new_hand_total)
+
+
+#Greeting
+greeting = input("Welcome to Casino Cat! To start the game enter s to quit game enter q!! Have fun!!")
+
+
+#Dealing with Aces
+if player_points[0] == 21:
+    print(f"\nTable View:\n\nPlayers Hand: {players_hand}\nPlayers Points: {player_points[0]}\nPlayer wins with Black Jack!!!")
+
+
+############################################################################W
+#  Get the value of a new card and append it to the values of original hand #
+#############################################################################
+######################################################################################################
+# 1st we append the values to the list below
+players_hand_initial_pair = []
+for i in values:
+    players_hand_initial_pair.append(i)
+
+#find the sum of the hand 
+players_total = sum(players_hand_initial_pair)
+
+print(f"Getting the values and appending them to a list: {players_hand_initial_pair}")
+ 
+# put in while loop 
+while players_total < 21: 
+# We get the values of a new card and append them to new card list 
+    new_card_list = []
+# define a new card 
+    new_card = deal_hand(shuffle_deck(), 1)
+#get the value of the new card 
+    new_card_value = new_card.values()
+# append the value to new card list 
+    for i in new_card_value:
+        new_card_list.append(i)
+# append the value of new card list to the initial pair list
+    players_hand_initial_pair.extend(new_card_list)
+# get the players total 
+    players_total = sum(players_hand_initial_pair)
+    print(f"The hand total in loop is: {players_total}")
+    
+######################################################################################################
+######################################################################################################
+# Using this to display to scree the hand and the total value of the hand. 
+#takes new_card variable and merges it to the existing hand
+    ph_add_card = add_card_to_hand(players_hand,new_card)
+#function above outputs two type of information the key value pair and just the values printing the values here 
+    print(f"printing the values for ph_add_card function {ph_add_card[1]}")
+######################################################################################################
